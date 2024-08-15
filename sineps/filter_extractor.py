fff16deee69a1b517cab2e34506c5b2f28a8b94d
Yimjaehyun93 <jhyim@cerestechs.com>
2024-08-15 13:38:34 +0900

    debug

--- sineps/filter_extractor.py
+++ sineps/filter_extractor.py
@@ -10,11 +10,5 @@ class FilterExtractorResponse:
         self.filter = self._get_filter()
 
     def _get_filter(self):
-        filter_str = self.data["result"]
-        try:
-            filter = json.loads(filter_str)
-        except json.JSONDecodeError as e:
-            raise FilterExtractorError(
-                "The filter in the response is not valid JSON"
-            ) from e
+        filter = self.data["result"]
         return filter
