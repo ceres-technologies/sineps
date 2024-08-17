65acc6daf2a4cba277f5bd9e28c84c05ae9841d3
Yimjaehyun93 <jhyim@cerestechs.com>
2024-08-17 12:25:40 +0900

    ver0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sineps",
-    version="0.0.12",
+    version="0.0.13",
     author="Ceres Technologies",
     author_email="infosec@sineps.io",
     description="The official Python library for the sineps API",
